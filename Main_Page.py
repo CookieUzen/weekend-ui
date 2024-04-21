b5f213089c0fdc7a05fc9034af2131539b47e8c1
Uzen Huang <uzen@cookieuz.io>
2024-04-21 01:46:05 -0500

    added inference to prediction ui

--- Main_Page.py
+++ Main_Page.py
@@ -10,20 +10,29 @@ else:
 
 def collect_user_data():
     '''Pops open a form to collect user data. Updates automatically'''
-    user_data = copy.copy(st.session_state.user_data)
+    user_data = dict()
 
     with st.form('user_data_form'):
-        user_data['Age'] = st.number_input("Enter your age", min_value=0, max_value=100, value=user_data['Age'])
-        user_data['Sleep Duration'] = st.number_input("Enter sleep duration in hours", min_value=0.0, max_value=24.0, value=user_data['Sleep Duration'], step=0.1)
-        user_data['Daily Steps'] = st.number_input("Enter your daily steps", min_value=0.0)
-        user_data['Gender'] = st.multiselect("Select your gender", ['Male', 'Female'])
-        #user_data['Quality of Sleep'] = st.slider("Enter sleep quality score", min_value=1, max_value=10, value=user_data['Quality of Sleep'], step=1)
-        #user_data['Sleep Disorder'] = st.multiselect("Select any sleep disorders you have", ['Insomnia', 'Sleep Apnea', 'Narcolepsy', 'None'], default=user_data['Sleep Disorder'])
+        user_data['sex'] = st.selectbox("Select your biological sex", ('Male', 'Female'))
+        user_data['age'] = st.number_input("Enter your age", min_value=18, max_value=100)
+        user_data['race'] = st.selectbox("Enter your race", ('American Indian/Alaskan Native', 'White', 'Black', 'Hispanic', 'Asian', 'Other'))
+        user_data['height'] = st.number_input("Enter your height in cm", min_value=1)
+        user_data['weight'] = st.number_input("Enter your weight in kg", min_value=1)
+        user_data['sleepTime'] = st.number_input("Enter sleep time in hours", min_value=0.0, max_value=24.0, step=0.5)
+        user_data['physicalActivity'] = st.selectbox("Do you do physical activity?", ('Yes', 'No'), index=0)
+        user_data['diabetic'] = st.selectbox("Are you diabetic?", ('Yes', 'No'), index=1)
+        user_data['smoking'] = st.selectbox("Do you smoke?", ('Yes', 'No'), index=1)
+        user_data['alcohol'] = st.selectbox("Do you drink alcohol?", ('Yes', 'No'), index=1)
+        user_data['stroke'] = st.selectbox("Have you had a stroke?", ('Yes', 'No'), index=1)
+        user_data['diabetes'] = st.selectbox("Do you have diabetes?", ('Yes', 'No'), index=1)
+        user_data['asthma'] = st.selectbox("Do you have asthma?", ('Yes', 'No'), index=1)
+        user_data['skinCancer'] = st.selectbox("Do you have skin cancer?", ('Yes', 'No'), index=1)
+        user_data['kidneyDisease'] = st.selectbox("Do you have kidney disease?", ('Yes', 'No'), index=1)
+        user_data['diffWalking'] = st.selectbox("Do you have difficulty walking?", ('Yes', 'No'), index=1)
 
         submitted = st.form_submit_button("Submit")
         if submitted:
             st.session_state.user_data = user_data
-
 
 st.set_page_config(
     page_title="Sleep Healthcare App",
@@ -33,9 +42,9 @@ st.header('Sleep Healthcare App')
 st.write("Upload and visualize data from the Sleep health and lifestyle dataset.")
 
 # Load some sample user data
-sample_data = {'Gender':'Male', 'Age': 29, 'Sleep Duration': 6.3, 'BMI Category': 'Normal', 'Blood Pressure': '126/83', 'Heart Rate': 77, 'Daily Steps': 5000}
-if 'user_data' not in st.session_state:
-    st.session_state.user_data = sample_data    
+# sample_data = {'Gender':'Male', 'Age': 29, 'Sleep Duration': 6.3, 'BMI Category': 'Normal', 'Blood Pressure': '126/83', 'Heart Rate': 77, 'Daily Steps': 5000}
+# if 'user_data' not in st.session_state:
+#     st.session_state.user_data = sample_data    
 
 collect_user_data()
 
